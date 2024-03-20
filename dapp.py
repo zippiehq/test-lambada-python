ca648c0fb384f21f8aabdb374fdd16f6a99fe493
root <Carsten Munk>
2024-03-20 10:37:25 +0000

    Add storing with IPFS
    Signed-off-by: root <Carsten Munk>

--- dapp.py
+++ dapp.py
@@ -1,6 +1,9 @@
 from os import environ
+import os
 import logging
 import requests
+import ipfshttpclient2
+from io import BytesIO
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
@@ -10,5 +13,28 @@ logger.info(f"HTTP rollup_server url is {rollup_server}")
 
 data = requests.get(rollup_server + "/get_tx")
 logger.info(f"Got tx {data.content}")
+
+ipfs_api = os.getenv('IPFS_API', '/ip4/127.0.0.1/tcp/5001')
+
+try:
+    ipfs_instance = ipfshttpclient2.connect(ipfs_api)
+except Exception as e:
+    print(f"Error connecting to IPFS API: {e}")
+    exit(1)
+
+content = "Hello World"
+directory_path = '/state'
+
+try:
+   ipfs_instance.files.mkdir(directory_path, parents=True)
+
+   output_path = f"{directory_path}/output.file"
+
+   ipfs_instance.files.write(output_path, BytesIO(content.encode('utf-8')), create=True, truncate=True)
+
+except Exception as e:
+    print(f"Error writing content to IPFS: {e}")
+
+
 
 requests.post(rollup_server + "/finish", json={})
